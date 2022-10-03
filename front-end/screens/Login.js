9bd7fd916109d2c948c8b5174e63f0d945737e65
chama97 <chamamanchanayake5@gmail.com>
2022-10-03 19:49:03 +0530

    sign done

--- front-end/screens/Login.js
+++ front-end/screens/Login.js
@@ -1,40 +1,42 @@
 import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { NativeBaseProvider, Text, Input, VStack, Button } from 'native-base'
-import { Alert } from 'react-native';
 
-export default function Login() {
+export default function Login({navigation}) {
 
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
 
 
-    const saveData = () => {
-        // fetch('https://jsonplaceholder.typicode.com/posts', {
-        //     method: 'POST',
-        //     body: JSON.stringify({
-        //         userName: userName,
-        //         password: password,
-        //     }),
-        //     headers: {
-        //         'Content-type': 'application/json; charset=UTF-8',
-        //     },
-        // })
-        //     .then((response) => {Alert.alert("Save Saved Successfully !")})
-        //     .catch((err)=>{Alert.alert("Error occured !")})
+    const checkLogin = () => {
+        fetch('http://localhost:4000/user', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: userName,
+                password: password,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => {
+                navigation.navigate("Home")
+                Alert.alert("Save Saved Successfully !")
+            })
+            .catch((err)=>{Alert.alert("Error occured !")})
     }
 
   return (
         <NativeBaseProvider>
             <View style={styles.container}>
-                <Text fontSize="3xl" bold  mt="10%" style={styles.lable}>LOGIN</Text>
-                <VStack space={5} alignItems="center" mt="15%">
+                <Text fontSize="4xl" bold color={'#1763d3'}  mt="20%" style={styles.lable}>LOGIN</Text>
+                <VStack space={6} alignItems="center" mt="15%">
                     <Input mx="3" value={userName} onChangeText={(e) => { setUserName(e) }} placeholder="User Name" w="80%" />
                     <Input mx="3" value={password} onChangeText={(e) => { setPassword(e) }} placeholder="Password" w="80%" />
-                    <Button style={styles.btn} size="lg" variant="solid" onPress={saveData}>
+                    <Button style={styles.btn} size="lg" variant="solid" onPress={checkLogin}>
                         Login
                     </Button>
-                    <Button style={styles.btn} size="md" variant="subtle" colorScheme="green" onPress={()=>{navigation.navigate("Sign")}}>
+                    <Button style={styles.btn} size="md" mt={2} variant="subtle" colorScheme="green" onPress={()=>{navigation.navigate("Sign")}}>
                         Create New Account
                     </Button>
                 </VStack>
--- front-end/screens/Sign.js
+++ front-end/screens/Sign.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { NativeBaseProvider, Text, Input, VStack, HStack, Button } from 'native-base'
+import { Alert } from 'react-native';
 
-export default function Sign() {
+    
+export default function Sign({navigation}) {
+
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [contact, setContact] = useState('');
+
+
+    const saveData = () => {
+        fetch('http://localhost:4000/user', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name,
+                address: address,
+                email: email,
+                password: password,
+                contact: contact,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => {Alert.alert("Save Saved Successfully !")})
+            .catch((err)=>{Alert.alert("Error occured !")})
+    }
+
   return (
-    <div>
-      
-    </div>
+    <NativeBaseProvider>
+            <View style={styles.container}>
+                <Text fontSize="4xl" bold color={'#1763d3'} mt="10%" style={styles.lable}>SIGN-UP</Text>
+                <VStack space={4} alignItems="center" mt="12%">
+                    <Input mx="3" value={name} onChangeText={(e) => { setName(e) }} placeholder="Name" w="80%" />
+                    <Input mx="3" value={address} onChangeText={(e) => { setAddress(e) }} placeholder="Address" w="80%" />
+                    <Input mx="3" value={email} onChangeText={(e) => { setEmail(e) }} placeholder="E-mail" w="80%" />
+                    <Input mx="3" value={password} onChangeText={(e) => { setPassword(e) }} placeholder="Password" w="80%" />
+                    <Input mx="3" value={contact} onChangeText={(e) => { setContact(e) }} placeholder="Contact" w="80%" />
+                    <HStack space={3} alignItems="center" mt="2%">
+                        <Button style={styles.btn} size="lg" variant="outline" onPress={saveData}>
+                            Cancel
+                        </Button>
+                        <Button style={styles.btn} size="lg" variant="solid" onPress={saveData}>
+                            Sign Up
+                        </Button>
+                    </HStack>
+                    <Button style={styles.btn} mt="3"  size="md" variant="subtle" colorScheme="green" onPress={()=>{navigation.navigate("Login")}}>
+                        Go to sign In
+                    </Button>
+                </VStack>
+            </View>
+        </NativeBaseProvider>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{  
+        alignItems:'center'
+    },
+    btn:{  
+        width:155,
+        fontSize:50,
+    },
+})
