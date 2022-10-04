eefdab54df3e9ae5a295cd75df02811d54511ade
chama97 <chamamanchanayake5@gmail.com>
2022-10-04 11:27:42 +0530

    car added

--- front-end/screens/Car.js
+++ front-end/screens/Car.js
@@ -1,10 +1,113 @@
-import React from 'react'
-import { View, Text, FlatList,TouchableOpacity, StyleSheet, Image, Button} from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, TouchableOpacity, Image, ToastAndroid } from 'react-native'
+import { NativeBaseProvider, Text, Input, VStack, HStack, Button } from 'native-base'
+import { Alert } from 'react-native';
+import { launchImageLibrary} from 'react-native-image-picker';
 
 export default function Car() {
+    const [type, setType] = useState('');
+    const [description, setDescription] = useState('');
+    const [photo, setPhoto] = useState([0]);
+
+
+    const saveData = () => {
+        fetch('http://localhost:4000/car', {
+            method: 'POST',
+            body: JSON.stringify({
+                type: type,
+                description: description,
+                photo: photo,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => {Alert.alert("Save Saved Successfully !")})
+            .catch((err)=>{Alert.alert("Error occured !")})
+    }
+
+    const setToastMessege = msg => {
+        ToastAndroid.showWithGravity(msg, ToastAndroid.SHORT, ToastAndroid.CENTER);
+    };
+
+    const uploadImage = () => {
+        let options = {
+            mediaType: 'photo',
+            quuality: 1,
+            includeBase64: true,
+        };
+        launchImageLibrary(options, response =>{
+            if(response.didCancel){
+                setToastMessege('Cancel image selection');
+            } else if(response.errorCode == 'permission'){
+                setToastMessege('Permission not satified');
+            } else if(response.errorCode == 'others'){
+                setToastMessege(response.errorMessage);
+            } else if(response.assets[0].fileSize > 2097152){
+                Alert.alert('Maximum image size exeeded')
+            }else{
+                setPhoto(response.assets[0].base64);
+            }
+        });
+    }
+
+    const removeImage = () => {
+        setPhoto('');
+        setToastMessege('Image Removed');
+    }
+
   return (
-   <View>
-    <Text fontSize="4xl" bold color={'#1763d3'}  mt="20%" >Car</Text>
-   </View>
+    <NativeBaseProvider>
+            <View style={styles.container}>
+                <Text fontSize="4xl" bold color={'#1763d3'} mt="10%" style={styles.lable}>Add Car</Text>
+                <VStack space={4} alignItems="center" mt="12%">
+                    <Input mx="3" value={type} onChangeText={(e) => { setType(e) }} placeholder="Type" w="80%" />
+                    <Input mx="3" value={description} onChangeText={(e) => { setDescription(e) }} placeholder="Description" w="80%" />
+                    <HStack space={3} alignItems="center" mt="4%">
+                        <TouchableOpacity style={styles.touchable} onPress={()=>{console.log("hello");}}>
+                            <Image source = {{uri:'data:Image/png;base64,' + photo}}
+                                style = {{ width: '99%', height: '98%', borderRadius:10, }}
+                            />
+                        </TouchableOpacity>
+                        <VStack space={4} alignItems="center">
+                            <Button  size="md" variant="solid" onPress={uploadImage}>
+                                Uploads
+                            </Button>
+                            <Button  size="md" variant="solid" colorScheme="error" onPress={removeImage}>
+                                Remove
+                            </Button>
+                        </VStack>
+                    </HStack>
+                    <HStack space={3} alignItems="center" mt="7%">
+                        <Button style={styles.btn} size="lg" variant="outline"  onPress={saveData}>
+                            Cancel
+                        </Button>
+                        <Button style={styles.btn} size="lg" variant="solid" onPress={saveData}>
+                            Save
+                        </Button>
+                    </HStack>
+                </VStack>
+            </View>
+        </NativeBaseProvider>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{  
+        alignItems:'center'
+    },
+    touchable:{  
+        height:150,
+        width :'60%',
+        borderColor:'#777788',
+        borderWidth:2,
+        backgroundColor:'#d6d6db',
+        borderRadius:10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    btn:{  
+        width:155,
+        fontSize:50,
+    },
+})
--- front-end/screens/Home.js
+++ front-end/screens/Home.js
@@ -1,12 +1,12 @@
-import { View, Text, FlatList,TouchableOpacity, StyleSheet, Image, Button} from 'react-native'
+import { View, Text, FlatList,TouchableOpacity, StyleSheet, Image, Button, ScrollView} from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 
 export default function Home({navigation}) {
   return (
-    <View style={{padding:50}}>
+    <ScrollView style={{padding:50}}>
         <View style={{marginBottom:30}}>
-            <Button style={{padding:5}} onPress={()=>{navigation.navigate("Car")}} title="Add New Vehicle">
+            <Button style={{height:30}} onPress={()=>{navigation.navigate("Car")}} title="Add New Vehicle">
                 Add New Vehicle
             </Button>
         </View>
@@ -42,8 +42,24 @@ export default function Home({navigation}) {
                 <Text style={{marginBottom:10, fontSize:14}} >ssssssssssssssssssssssssss</Text>
             </View>
         </View>
+
+        <View style={styles.container}>
+            <View style={{flex:2, backgroundColor:'#fff', alignItems:'center', justifyContent:'center'}}>
+                <Text style={{marginBottom:10,fontWeight:'bold',fontSize:17}} >BMW 760L 7Series</Text>
+            </View>
+            <View style={{flex:6,  backgroundColor:'#e600e6'}}>
+                <TouchableOpacity style={{height:'100%', backgroundColor:'#ffff'}} onPress={()=>{console.log("hello");}}>
+                    <Image source = {{uri:'https://images.hindustantimes.com/auto/img/2022/01/15/1600x900/Lamborghini_1642220975552_1642220982866.jpg'}}
+                        style = {{ width: '100%', height: '100%' }}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View style={{flex:2,  backgroundColor:'#fff', alignItems:'center', justifyContent:'center'}}>
+                <Text style={{marginBottom:10, fontSize:14}} >ssssssssssssssssssssssssss</Text>
+            </View>
+        </View>
     
-    </View>
+    </ScrollView>
   )
 }
 
